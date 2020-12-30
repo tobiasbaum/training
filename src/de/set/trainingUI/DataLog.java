27804fd3491dbfc5430f43081884b4eec363f5f5
Tobias Baum <tbaum@tntinteractive.de>
2020-12-30 13:52:37 +0100

    rolling logs

--- src/de/set/trainingUI/DataLog.java
+++ src/de/set/trainingUI/DataLog.java
@@ -6,8 +6,11 @@ import java.io.OutputStream;
 
 public class DataLog {
 
+	private static final long ONE_DAY = 24L * 60 * 60 * 1000;
+
     private static final Object LOCK = new Object();
     private static OutputStream stream;
+    private static long lastOpen;
 
     public static void log(final long experimentId, final String message) {
         try {
@@ -15,8 +18,13 @@ public class DataLog {
             final String fullString = experimentId + ";" + time + ";" + message.replace('\n', ' ').replace('\r', ' ') + "\n";
             final byte[] msg = fullString.getBytes("UTF-8");
             synchronized (LOCK) {
+            	if (stream != null && time - lastOpen > ONE_DAY) {
+            		stream.close();
+            		stream = null;
+            	}
                 if (stream == null) {
                     stream = new FileOutputStream("experiment." + time + ".log");
+                    lastOpen = time;
                 }
                 stream.write(msg);
                 stream.flush();
