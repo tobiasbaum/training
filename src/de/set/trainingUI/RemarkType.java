a62847d6a6cc58eb36f0347307e13e43ecb747be
Tobias Baum <tbaum@tntinteractive.de>
2020-12-31 11:33:17 +0100

    Tooltips für Fehlerkategorien

--- src/de/set/trainingUI/RemarkType.java
+++ src/de/set/trainingUI/RemarkType.java
@@ -1,16 +1,19 @@
 package de.set.trainingUI;
 
 public enum RemarkType {
-    WRONG_COMPARISON("Fehlerhafte(r) Vergleich/Bedingung"),
-    WRONG_CALCULATION("Fehlerhafte Berechnung"),
-    MISSING_CODE("Fehlender Code"),
-    OTHER_ALGORITHMIC_PROBLEM("Anderes algorithmisches Problem"),
-    DUPLICATE_CODE("Doppelter Code");
+    WRONG_COMPARISON("Fehlerhafte(r) Vergleich/Bedingung", "Eine Bedingung (in if, while, ...) oder ein Vergleich (<, >, ==, ...) ist falsch, sodass sich das Programm inkorrekt verhält."),
+    WRONG_CALCULATION("Fehlerhafte Berechnung", "Eine Berechnung (+, -, +=, ...) ist falsch, sodass sich das Programm inkorrekt verhält."),
+    WRONG_MESSAGE("Fehlerhafte Meldung", "Eine Meldung für den Benutzer (Exception, Ausgabe, ...) ist falsch oder unvollständig."),
+    MISSING_CODE("Fehlender Code", "Es fehlt Code (fehlendes Statement / Expression / Keyword / Fallunterscheidung / ...), sodass sich das Programm inkorrekt verhält."),
+    OTHER_ALGORITHMIC_PROBLEM("Anderes Korrektheitsproblem", "Der Code in der Zeile sorgt dafür, dass sich das Programm inkorrekt verhält und es trifft keine der anderen Problemkategorien zu."),
+    DUPLICATE_CODE("Doppelter Code", "Es wurde in größerem Umfang Code dupliziert, sodass ein Wartbarkeitsproblem vorliegt.");
 
     private String text;
+    private String description;
 
-    private RemarkType(final String text) {
+    private RemarkType(final String text, String description) {
         this.text = text;
+        this.description = description;
     }
 
     public String getValue() {
@@ -19,5 +22,9 @@ public enum RemarkType {
 
     public String getText() {
         return this.text;
+    }
+
+    public String getDescription() {
+    	return this.description;
     }
 }
