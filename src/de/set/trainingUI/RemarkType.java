fa04495f11a1d73bbabe56fb9afc3bab27f3a5ac
Tobias Baum <tbaum@tntinteractive.de>
2021-06-09 20:46:55 +0200

    Reihenfolge der Remark-Typen geändert, um "Fehlerhaft" und "Fehlend" besser abzugrenzen

--- src/de/set/trainingUI/RemarkType.java
+++ src/de/set/trainingUI/RemarkType.java
@@ -4,9 +4,9 @@ public enum RemarkType {
     WRONG_COMPARISON("Fehlerhafte(r) Vergleich/Bedingung", "Eine Bedingung (in if, while, ...) oder ein Vergleich (<, >, ==, ...) ist falsch, sodass sich das Programm inkorrekt verhält."),
     WRONG_CALCULATION("Fehlerhafte Berechnung", "Eine Berechnung (+, -, +=, ...) ist falsch, sodass sich das Programm inkorrekt verhält."),
     WRONG_MESSAGE("Fehlerhafte Meldung", "Eine Meldung für den Benutzer (Exception, Ausgabe, ...) ist falsch oder unvollständig."),
+    DUPLICATE_CODE("Doppelter Code", "Es wurde in größerem Umfang Code dupliziert, sodass ein Wartbarkeitsproblem vorliegt."),
     MISSING_CODE("Fehlender Code", "Es fehlt Code (fehlendes Statement / Expression / Keyword / Fallunterscheidung / ...), sodass sich das Programm inkorrekt verhält."),
-    OTHER_ALGORITHMIC_PROBLEM("Anderes Korrektheitsproblem", "Der Code in der Zeile sorgt dafür, dass sich das Programm inkorrekt verhält und es trifft keine der anderen Problemkategorien zu."),
-    DUPLICATE_CODE("Doppelter Code", "Es wurde in größerem Umfang Code dupliziert, sodass ein Wartbarkeitsproblem vorliegt.");
+    OTHER_ALGORITHMIC_PROBLEM("Anderes Korrektheitsproblem", "Der Code in der Zeile sorgt dafür, dass sich das Programm inkorrekt verhält und es trifft keine der anderen Problemkategorien zu.");
 
     private String text;
     private String description;
